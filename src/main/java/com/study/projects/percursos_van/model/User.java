dfc7f512488725d6ed401de8f8360bce608353fb
Lucas <lucas.hen.lps@gmail.com>
2025-02-23 01:39:38 -0300

    feat(user): create User entity and Role enum

---
+++ src/main/java/com/study/projects/percursos_van/model/User.java
@@ -0,0 +1,62 @@
+package com.study.projects.percursos_van.model;
+
+import com.study.projects.percursos_van.web.controller.dto.user.UserCreateDTO;
+import com.study.projects.percursos_van.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "full_name", nullable = false, length = 50)
+    private String fullName;
+
+    @Column(name = "email", nullable = false, length = 200, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    public User(UserCreateDTO dto) {
+        this.fullName = dto.fullName();
+        this.email = dto.email();
+        this.password = dto.password();
+        this.role = Role.valueOf(dto.role());
+    }
+
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createdBy;
+    @Column(name = "last_modified_by")
+    @LastModifiedBy
+    private String lastModifiedBy;
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @Column(name = "last_modified_at")
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+}
---
+++ src/main/java/com/study/projects/percursos_van/model/enums/Role.java
@@ -0,0 +1,5 @@
+package com.study.projects.percursos_van.model.enums;
+
+public enum Role {
+    DRIVER, ADMIN, STUDENT
+}
