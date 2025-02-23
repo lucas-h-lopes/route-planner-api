13aecbd37ffb470f111b5b893e6bc35c3ac11ebd
Lucas <lucas.hen.lps@gmail.com>
2025-02-23 01:45:29 -0300

    feat(user): add UserMapper

---
+++ src/main/java/com/study/projects/percursos_van/web/mapper/user/UserMapper.java
@@ -0,0 +1,24 @@
+package com.study.projects.percursos_van.web.mapper.user;
+
+import com.study.projects.percursos_van.model.User;
+import com.study.projects.percursos_van.web.controller.dto.user.UserCreateDTO;
+import com.study.projects.percursos_van.web.controller.dto.user.UserResponseDTO;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+public class UserMapper {
+
+    public static User toUser(UserCreateDTO dto){
+        return new User(dto);
+    }
+
+    public static UserResponseDTO toResponse(User user){
+        return new UserResponseDTO(user);
+    }
+
+    public static List<UserResponseDTO> toResponseList(List<User> users){
+        return users.stream().map(UserMapper::toResponse).toList();
+    }
+}
