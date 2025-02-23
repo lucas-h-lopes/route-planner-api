656d364f7139c3ad99a7ad585fa3f6760e09d807
Lucas <lucas.hen.lps@gmail.com>
2025-02-23 00:41:05 -0300

    initial commit

---
+++ src/main/java/com/study/projects/percursos_van/PercursosVanApplication.java
@@ -0,0 +1,13 @@
+package com.study.projects.percursos_van;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PercursosVanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PercursosVanApplication.class, args);
+	}
+
+}
