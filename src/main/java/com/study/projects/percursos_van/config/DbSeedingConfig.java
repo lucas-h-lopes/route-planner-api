b0564d2ffeec4ca102912a034920f4f78d975f2b
Lucas <lucas.hen.lps@gmail.com>
2025-02-23 00:45:40 -0300

    feat(db): add initial db seeding for login as administrator

---
+++ src/main/java/com/study/projects/percursos_van/config/DbSeedingConfig.java
@@ -0,0 +1,31 @@
+package com.study.projects.percursos_van.config;
+
+import com.study.projects.percursos_van.model.User;
+import com.study.projects.percursos_van.model.enums.Role;
+import com.study.projects.percursos_van.repository.UserRepository;
+import com.study.projects.percursos_van.service.UserService;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class DbSeedingConfig {
+
+    private final UserRepository userRepository;
+    private final UserService userService;
+    private final String email = "admin@email.com";
+
+    @PostConstruct
+    private void insertAdmin() {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword("123456");
+        user.setRole(Role.ADMIN);
+        user.setFullName("user administrator");
+
+        if (!userRepository.existsByEmail(email)) {
+            userService.insert(user);
+        }
+    }
+}
