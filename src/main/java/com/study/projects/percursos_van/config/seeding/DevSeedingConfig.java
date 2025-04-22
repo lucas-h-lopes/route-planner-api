1dc0091098f4393416044b9d7eb7499c70d07d81
Lucas <lucas.hen.lps@gmail.com>
2025-04-22 00:57:31 -0300

    fix: change db seeding to contemplate phone changes

--- src/main/java/com/study/projects/percursos_van/config/seeding/DevSeedingConfig.java
+++ src/main/java/com/study/projects/percursos_van/config/seeding/DevSeedingConfig.java
@@ -1,7 +1,11 @@
 package com.study.projects.percursos_van.config.seeding;
 
-import com.study.projects.percursos_van.model.User;
+import com.study.projects.percursos_van.builder.AddressBuilder;
+import com.study.projects.percursos_van.model.*;
 import com.study.projects.percursos_van.model.enums.Role;
+import com.study.projects.percursos_van.service.AddressService;
+import com.study.projects.percursos_van.service.DriverService;
+import com.study.projects.percursos_van.service.StudentService;
 import com.study.projects.percursos_van.service.UserService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +14,7 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Random;
 
 @Profile("dev")
@@ -20,6 +25,9 @@ public class DevSeedingConfig {
 
     private final Random random;
     private final UserService userService;
+    private final DriverService driverService;
+    private final StudentService studentService;
+
     private final String[] fullNamesArr = new String[]{
             "Lucas Henrique Lopes",
             "Anthony Miguel Samuel da Cruz",
@@ -52,21 +60,80 @@ public class DevSeedingConfig {
             user.setFullName(fullNamesArr[random.nextInt(0, 15)]);
             user.setEmail("testeseeding" + (x + 1) + "@email.com");
             user.setPassword("12345678aA@");
-            user.setCreatedBy("System");
             user.setCreatedAt(LocalDateTime.now());
-            user.setLastModifiedBy("System");
             user.setLastModifiedAt(LocalDateTime.now());
-            user.setCpf(randomNumbers());
+            user.setCpf(randomNumbers(11));
+            setCreatedAndModified(user);
 
-            userService.insert(user);
+            switch (user.getRole()) {
+                case Role.DRIVER -> {
+                    Driver driver = new Driver();
+                    user.setPhones(
+                            Collections.singletonList(new Phone(randomNumbers(11), Phone.PhoneType.PRINCIPAL, user))
+                    );
+                    setCreatedAndModified(user.getPhones().getFirst());
+                    driver.setUser(user);
+                    setCreatedAndModified(driver);
+                    driverService.insert(driver);
+                }
+                case Role.STUDENT -> {
+                    Student student = new Student();
+                    Address address = AddressBuilder.withDefaultAddress().get();
+                    address.setStudent(student);
+
+                    student.getAddresses().add(address);
+
+                    setCreatedAndModified(student);
+
+                    student.setUser(user);
+                    studentService.insert(student);
+                }
+                default -> userService.insert(user);
+            }
         }
     }
 
-    private String randomNumbers(){
+    private String generateRandomPhoneNumber() {
+        Random random = new Random();
+        StringBuilder rawPhone = new StringBuilder();
+        for (int x = 0; x < 10; x++) {
+            rawPhone.append(randomNumbers(11));
+        }
+        return rawPhone.toString();
+    }
+
+    private String randomNumbers(int times) {
         StringBuilder sb = new StringBuilder();
-        for(int x = 0; x<11; x++){
-            sb.append(random.nextInt(0,10));
+        for (int x = 0; x < times; x++) {
+            sb.append(random.nextInt(0, 10));
         }
         return sb.toString();
+    }
+
+    private void setCreatedAndModified(Driver driver) {
+        driver.setLastModifiedBy("System");
+        driver.setCreatedBy("System");
+    }
+
+    private void setCreatedAndModified(Student student) {
+        student.setLastModifiedBy("System");
+        student.setCreatedBy("System");
+
+        if (!student.getAddresses().isEmpty()) {
+            for (Address address : student.getAddresses()) {
+                address.setLastModifiedBy("System");
+                address.setCreatedBy("System");
+            }
+        }
+    }
+
+    private void setCreatedAndModified(User user) {
+            user.setLastModifiedBy("System");
+            user.setCreatedBy("System");
+    }
+
+    private void setCreatedAndModified(Phone phone){
+            phone.setCreatedBy("System");
+            phone.setLastModifiedBy("System");
     }
 }
